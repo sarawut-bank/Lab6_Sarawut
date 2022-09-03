25c2f266370402bbb2e47ef2d9759352ed5bffad
sarawut-bank <sarwut123zz@gmail.com>
2022-09-03 20:10:02 +0700

    เชื่อมแอพกับ firebase

--- lib/screen/formscreen.dart
+++ lib/screen/formscreen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -14,95 +15,121 @@ class FormScreen extends StatefulWidget {
 class _FormScreenState extends State<FormScreen> {
   final formKey = GlobalKey<FormState>();
   Student myStudent = Student();
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("แบบฟอร์มบันทึกคะแนนสอบ"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "ชื่อ",
-                  style: TextStyle(fontSize: 20),
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Error"),
+              ),
+              body: Center(
+                child: Text("${snapshot.error}"),
+              ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("แบบฟอร์มบันทึกคะแนนสอบ"),
+              ),
+              body: Container(
+                padding: EdgeInsets.all(20),
+                child: Form(
+                  key: formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "ชื่อ",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: "กรุณาป้อนชื่อ!!"),
+                          onSaved: (String? fname) {
+                            myStudent.fname = fname;
+                          },
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "นามสกุล",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          validator: RequiredValidator(
+                              errorText: "กรุณาป้อนนามสกุล!!"),
+                          onSaved: (String? lname) {
+                            myStudent.lname = lname;
+                          },
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "อีเมล",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          validator: MultiValidator([
+                            EmailValidator(
+                                errorText: "รูปแบบอีเมลไม่ถูกต้อง!!"),
+                            RequiredValidator(errorText: "กรุณาป้อนอีเมล!!")
+                          ]),
+                          onSaved: (String? email) {
+                            myStudent.email = email;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "คะแนนสอบ",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: "กรุณาป้อนคะแนน!!"),
+                          onSaved: (String? score) {
+                            myStudent.score = score;
+                          },
+                          keyboardType: TextInputType.number,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              child: Text(
+                                "บันทึกข้อมูล",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              onPressed: () {
+                                if (formKey.currentState!.validate()) {
+                                  formKey.currentState?.save();
+                                  print(
+                                      "ข้อมูล = ${myStudent.fname}${myStudent.lname}${myStudent.email}${myStudent.score}");
+                                }
+                              }),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "กรุณาป้อนชื่อ!!"),
-                  onSaved: (String? fname) {
-                    myStudent.fname = fname;
-                  },
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "นามสกุล",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "กรุณาป้อนนามสกุล!!"),
-                  onSaved: (String? lname) {
-                    myStudent.lname = lname;
-                  },
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "อีเมล",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  validator: MultiValidator([
-                    EmailValidator(errorText: "รูปแบบอีเมลไม่ถูกต้อง!!"),
-                    RequiredValidator(errorText: "กรุณาป้อนอีเมล!!")
-                  ]),
-                  onSaved: (String? email) {
-                    myStudent.email = email;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  "คะแนนสอบ",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(
-                  validator: RequiredValidator(errorText: "กรุณาป้อนคะแนน!!"),
-                  onSaved: (String? score) {
-                    myStudent.score = score;
-                  },
-                  keyboardType: TextInputType.number,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      child: Text(
-                        "บันทึกข้อมูล",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState?.save();
-                          print(
-                              "ข้อมูล = ${myStudent.fname}${myStudent.lname}${myStudent.email}${myStudent.score}");
-                        }
-                      }),
-                ),
-              ],
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        });
   }
 }
